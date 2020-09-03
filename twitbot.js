853423292086e6e61dcc7a7bd1d087bb78146b26
Ryan Sharrar <rshar@umich.edu>
2020-09-02 20:31:07 -0400

    can tweet now

--- twitbot.js
+++ twitbot.js
@@ -1,4 +1,26 @@
-import {getSecret} from "./keys";
+import {getSecret, getKey, getToken, getTokenSecret} from "./keys";
+import Twit from "twit";
+import { writeFile } from "fs";
 
-console.log("Program Me Daddy UwU");
-console.log(getSecret());
+let toText;
+
+let user = new Twit({
+    consumer_key: getKey(),
+    consumer_secret: getSecret(),
+    access_token: getToken(),
+    access_token_secret: getTokenSecret()
+})
+
+function twitterPost(postText) {
+    user.post('statuses/update', {status: postText}, function(err, data, response){
+        console.log("Posted: ",data.text)
+    })
+}
+
+user.get('statuses/user_timeline', {id: 'realDonaldTrump'}, function(err,data,response){
+    //console.log(data);
+    toText = data;
+    writeFile('./output.txt', toText, function(){console.log('done')});
+})
+
+//twitterPost("test2");
