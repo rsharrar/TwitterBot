043c457f59da35e4d95ad9b8e8838f3ea2f15397
Ryan Sharrar <rshar@umich.edu>
2020-09-07 14:26:30 -0400

    bot working without machine learning

--- twitbot.js
+++ twitbot.js
@@ -1,8 +1,6 @@
 import {getSecret, getKey, getToken, getTokenSecret} from "./keys";
 import Twit from "twit";
-
-
-let toText;
+import { stringify } from "querystring";
 
 let user = new Twit({
     consumer_key: getKey(),
@@ -17,12 +15,42 @@ function twitterPost(postText) {
     })
 }
 
+function schmeseIt(word){
+    let frontText = ''
+    if(word){
+        if(word[0] === word[0].toUpperCase()){
+            frontText = 'Schm'
+        }else{
+            frontText = 'schm'
+        }
+
+        let wordLen = word.length
+        for(let i = 0; i < wordLen; i++){
+            if((word[i] == 'a') || 
+            (word[i] == 'a') || 
+            (word[i] == 'e') || 
+            (word[i] == 'i') || 
+            (word[i] == 'o') || 
+            (word[i] == 'u') || 
+            (word[i] == 'y')){
+                let backText = word.substring(i,(wordLen))
+                let output = frontText + backText
+                return output
+            }
+        }
+        return word
+    }
+}
+
 user.get('statuses/user_timeline', {id: 'realDonaldTrump', count: 4, tweet_mode: 'extended'}, function(err,data,response){
+    console.log(data)
     console.log(data[0]['full_text']);
     console.log(data[1]['full_text']);
-    toText = data;
-    //writeFile('./output.txt', JSON.stringify(toText), function(){console.log('done')});
+    let inputText = data[0]['full_text'];
+    let inputarray = inputText.split(" ");
+    let outputText = [];
+    for(let i = 0; i < inputarray.length; i++){
+        outputText.push(schmeseIt(inputarray[i]))
+    }
+    twitterPost(outputText)
 })
-
-
-//twitterPost("test2");
